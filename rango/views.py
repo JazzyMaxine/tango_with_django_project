b91ce3a293336ceeb277e120a3c3ca1d0e4fdec0
jazzymaxine <128296928+JazzyMaxine@users.noreply.github.com>
2024-02-02 14:56:46 +0000

    chapter 4

--- rango/views.py
+++ rango/views.py
@@ -10,4 +10,7 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {
+        "boldmessage": "This tutorial has been put together by Maxine.",
+    }
+    return render(request, "rango/about.html", context=context_dict)
